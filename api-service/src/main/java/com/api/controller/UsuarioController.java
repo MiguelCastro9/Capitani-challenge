4ef8f8e2f5d2b618303de8e6e0992a272691ae13
MiguelCastro9 <miguel.castro9@hotmail.com>
2023-02-15 16:41:12 -0300

    ajustes e regra de endpoint

--- api-service/src/main/java/com/api/controller/UsuarioController.java
+++ api-service/src/main/java/com/api/controller/UsuarioController.java
@@ -4,11 +4,16 @@ import com.api.dto.UsuarioRequestDto;
 import com.api.dto.UsuarioResponseDto;
 import com.api.model.UsuarioModel;
 import com.api.service.UsuarioService;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +21,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  *
@@ -29,9 +37,34 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @PostMapping("/{username}")
-    public ResponseEntity<UsuarioResponseDto> salvar(@PathVariable String username, @RequestBody UsuarioRequestDto usuarioRequestDto) {
+    public UsuarioRequestDto apiGithub(String username, UsuarioRequestDto usuarioRequestDto) {
 
+        RestTemplate restTemplate = new RestTemplate();
+        UriComponents uri = UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host("api.github.com")
+                .path("users")
+                .queryParam(username)
+                .build();
+        
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+		messageConverters.add(converter);
+		restTemplate.setMessageConverters(messageConverters);
+
+        ResponseEntity<UsuarioRequestDto> entity = restTemplate.getForEntity(uri.toUriString(), UsuarioRequestDto.class);
+        
+
+
+        return usuarioRequestDto;
+    }
+
+    @PostMapping("/{username}")
+    public ResponseEntity<UsuarioResponseDto> salvar(@PathVariable String username, UsuarioRequestDto usuarioRequestDto) {
+
+        apiGithub(username, usuarioRequestDto);
+        System.out.println(usuarioRequestDto.toString());
         UsuarioModel usuario = usuarioService.salvar(usuarioRequestDto.converterUsuarioDtoParaEntidade());
         return new ResponseEntity<UsuarioResponseDto>(UsuarioResponseDto.converterEntidadeParaUsuarioDto(usuario), HttpStatus.CREATED);
     }
--- api-service/src/main/java/com/api/dto/UsuarioResponseDto.java
+++ api-service/src/main/java/com/api/dto/UsuarioResponseDto.java
@@ -1,7 +1,6 @@
 package com.api.dto;
 
 import com.api.model.UsuarioModel;
-import java.util.Date;
 
 /**
  *
@@ -9,49 +8,97 @@ import java.util.Date;
  */
 public class UsuarioResponseDto {
 
-    private Long id;
+    private Long idUsuario;
     private String login;
+    private Long id;
+    private String node_id;
     private String avatar_url;
+    private String gravatar_id;
+    private String url;
+    private String html_url;
+    private String followers_url;
+    private String following_url;
+    private String gists_url;
+    private String starred_url;
+    private String subscriptions_url;
+    private String organizations_url;
+    private String repos_url;
+    private String events_url;
+    private String received_events_url;
+    private String type;
+    private boolean site_admin;
     private String name;
     private String company;
     private String blog;
     private String location;
     private String email;
+    private String hireable;
     private String bio;
-    private Integer followers;
-    private Integer following;
-    private Date created_at;
-    private Date updated_at;
+    private String twitter_username;
+    private Long public_repos;
+    private Long public_gists;
+    private Long followers;
+    private Long following;
+    private String created_at;
+    private String updated_at;
 
-    public UsuarioResponseDto(Long id, String login, String avatar_url, String name, String company, String blog, String location, String email, String bio, Integer followers, Integer following, Date created_at, Date updated_at) {
-        this.id = id;
+    public UsuarioResponseDto(Long idUsuario, String login, Long id, String node_id, String avatar_url, String gravatar_id, String url, String html_url, String followers_url, String following_url, String gists_url, String starred_url, String subscriptions_url, String organizations_url, String repos_url, String events_url, String received_events_url, String type, boolean site_admin, String name, String company, String blog, String location, String email, String hireable, String bio, String twitter_username, Long public_repos, Long public_gists, Long followers, Long following, String created_at, String updated_at) {
+        this.idUsuario = idUsuario;
         this.login = login;
+        this.id = id;
+        this.node_id = node_id;
         this.avatar_url = avatar_url;
+        this.gravatar_id = gravatar_id;
+        this.url = url;
+        this.html_url = html_url;
+        this.followers_url = followers_url;
+        this.following_url = following_url;
+        this.gists_url = gists_url;
+        this.starred_url = starred_url;
+        this.subscriptions_url = subscriptions_url;
+        this.organizations_url = organizations_url;
+        this.repos_url = repos_url;
+        this.events_url = events_url;
+        this.received_events_url = received_events_url;
+        this.type = type;
+        this.site_admin = site_admin;
         this.name = name;
         this.company = company;
         this.blog = blog;
         this.location = location;
         this.email = email;
+        this.hireable = hireable;
         this.bio = bio;
+        this.twitter_username = twitter_username;
+        this.public_repos = public_repos;
+        this.public_gists = public_gists;
         this.followers = followers;
         this.following = following;
         this.created_at = created_at;
         this.updated_at = updated_at;
     }
 
+    //Método para converter entidade em DTO.
     public static UsuarioResponseDto converterEntidadeParaUsuarioDto(UsuarioModel usuarioModel) {
-        return new UsuarioResponseDto(usuarioModel.getId(), usuarioModel.getLogin(), usuarioModel.getAvatar_url(),
-                usuarioModel.getName(), usuarioModel.getCompany(), usuarioModel.getBlog(), usuarioModel.getLocation(),
-                usuarioModel.getEmail(), usuarioModel.getBio(), usuarioModel.getFollowers(), usuarioModel.getFollowing(),
+        return new UsuarioResponseDto(usuarioModel.getId(), usuarioModel.getLogin(), usuarioModel.getId(),
+                usuarioModel.getNode_id(), usuarioModel.getAvatar_url(), usuarioModel.getGravatar_id(),
+                usuarioModel.getUrl(), usuarioModel.getHtml_url(), usuarioModel.getFollowers_url(),
+                usuarioModel.getFollowing_url(), usuarioModel.getGists_url(), usuarioModel.getStarred_url(),
+                usuarioModel.getSubscriptions_url(), usuarioModel.getOrganizations_url(), usuarioModel.getRepos_url(),
+                usuarioModel.getEvents_url(), usuarioModel.getReceived_events_url(), usuarioModel.getType(),
+                usuarioModel.isSite_admin(), usuarioModel.getName(), usuarioModel.getCompany(),
+                usuarioModel.getBlog(), usuarioModel.getLocation(), usuarioModel.getEmail(), usuarioModel.getHireable(),
+                usuarioModel.getBio(), usuarioModel.getTwitter_username(), usuarioModel.getPublic_repos(),
+                usuarioModel.getPublic_gists(), usuarioModel.getFollowers(), usuarioModel.getFollowing(),
                 usuarioModel.getCreated_at(), usuarioModel.getUpdated_at());
     }
 
-    public Long getId() {
-        return id;
+    public Long getIdUsuario() {
+        return idUsuario;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
     }
 
     public String getLogin() {
@@ -62,12 +109,140 @@ public class UsuarioResponseDto {
         this.login = login;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNode_id() {
+        return node_id;
+    }
+
+    public void setNode_id(String node_id) {
+        this.node_id = node_id;
+    }
+
     public String getAvatar_url() {
         return avatar_url;
     }
 
     public void setAvatar_url(String avatar_url) {
         this.avatar_url = avatar_url;
+    }
+
+    public String getGravatar_id() {
+        return gravatar_id;
+    }
+
+    public void setGravatar_id(String gravatar_id) {
+        this.gravatar_id = gravatar_id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public void setHtml_url(String html_url) {
+        this.html_url = html_url;
+    }
+
+    public String getFollowers_url() {
+        return followers_url;
+    }
+
+    public void setFollowers_url(String followers_url) {
+        this.followers_url = followers_url;
+    }
+
+    public String getFollowing_url() {
+        return following_url;
+    }
+
+    public void setFollowing_url(String following_url) {
+        this.following_url = following_url;
+    }
+
+    public String getGists_url() {
+        return gists_url;
+    }
+
+    public void setGists_url(String gists_url) {
+        this.gists_url = gists_url;
+    }
+
+    public String getStarred_url() {
+        return starred_url;
+    }
+
+    public void setStarred_url(String starred_url) {
+        this.starred_url = starred_url;
+    }
+
+    public String getSubscriptions_url() {
+        return subscriptions_url;
+    }
+
+    public void setSubscriptions_url(String subscriptions_url) {
+        this.subscriptions_url = subscriptions_url;
+    }
+
+    public String getOrganizations_url() {
+        return organizations_url;
+    }
+
+    public void setOrganizations_url(String organizations_url) {
+        this.organizations_url = organizations_url;
+    }
+
+    public String getRepos_url() {
+        return repos_url;
+    }
+
+    public void setRepos_url(String repos_url) {
+        this.repos_url = repos_url;
+    }
+
+    public String getEvents_url() {
+        return events_url;
+    }
+
+    public void setEvents_url(String events_url) {
+        this.events_url = events_url;
+    }
+
+    public String getReceived_events_url() {
+        return received_events_url;
+    }
+
+    public void setReceived_events_url(String received_events_url) {
+        this.received_events_url = received_events_url;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isSite_admin() {
+        return site_admin;
+    }
+
+    public void setSite_admin(boolean site_admin) {
+        this.site_admin = site_admin;
     }
 
     public String getName() {
@@ -110,6 +285,14 @@ public class UsuarioResponseDto {
         this.email = email;
     }
 
+    public String getHireable() {
+        return hireable;
+    }
+
+    public void setHireable(String hireable) {
+        this.hireable = hireable;
+    }
+
     public String getBio() {
         return bio;
     }
@@ -118,35 +301,60 @@ public class UsuarioResponseDto {
         this.bio = bio;
     }
 
-    public Integer getFollowers() {
+    public String getTwitter_username() {
+        return twitter_username;
+    }
+
+    public void setTwitter_username(String twitter_username) {
+        this.twitter_username = twitter_username;
+    }
+
+    public Long getPublic_repos() {
+        return public_repos;
+    }
+
+    public void setPublic_repos(Long public_repos) {
+        this.public_repos = public_repos;
+    }
+
+    public Long getPublic_gists() {
+        return public_gists;
+    }
+
+    public void setPublic_gists(Long public_gists) {
+        this.public_gists = public_gists;
+    }
+
+    public Long getFollowers() {
         return followers;
     }
 
-    public void setFollowers(Integer followers) {
+    public void setFollowers(Long followers) {
         this.followers = followers;
     }
 
-    public Integer getFollowing() {
+    public Long getFollowing() {
         return following;
     }
 
-    public void setFollowing(Integer following) {
+    public void setFollowing(Long following) {
         this.following = following;
     }
 
-    public Date getCreated_at() {
+    public String getCreated_at() {
         return created_at;
     }
 
-    public void setCreated_at(Date created_at) {
+    public void setCreated_at(String created_at) {
         this.created_at = created_at;
     }
 
-    public Date getUpdated_at() {
+    public String getUpdated_at() {
         return updated_at;
     }
 
-    public void setUpdated_at(Date updated_at) {
+    public void setUpdated_at(String updated_at) {
         this.updated_at = updated_at;
     }
+
 }
